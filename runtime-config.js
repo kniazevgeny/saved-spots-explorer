ceafe6fed5bb8b0ac363e37f5bed97c403c81d20
Evgeny Kniazev <41857481+kniazevgeny@users.noreply.github.com>
2026-08-03 01:44:43 +0200

    Initialize WHIMBEAN_CONFIG with API URL and token
    w domain-restricted token

--- runtime-config.js
+++ runtime-config.js
@@ -1,1 +1,4 @@
-window.__WHIMBEAN_CONFIG__ = window.__WHIMBEAN_CONFIG__ || {};
+window.__WHIMBEAN_CONFIG__ = {
+    apiUrl: 'https://api.whimbean.com',
+    mapboxToken: 'pk.eyJ1Ijoia25pYXpldmdlbnkiLCJhIjoiY21zY2Z5amt4MG9lOTJ4cXQxM2phOWJiZSJ9.e6MWfw6ykMAtXWi70Qh7oQ',
+  };
