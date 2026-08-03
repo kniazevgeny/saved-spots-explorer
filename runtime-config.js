a31db0760bf844747584d71d6b4cd201154470cf
Evgeny Kniazev <eugene.kniazev@gmail.com>
2026-08-03 02:22:11 +0200

    Publish saved spots analytics build

--- runtime-config.js
+++ runtime-config.js
@@ -1,4 +1,6 @@
 window.__WHIMBEAN_CONFIG__ = {
-    apiUrl: 'https://api.whimbean.com',
-    mapboxToken: 'pk.eyJ1Ijoia25pYXpldmdlbnkiLCJhIjoiY21zY2Z5amt4MG9lOTJ4cXQxM2phOWJiZSJ9.e6MWfw6ykMAtXWi70Qh7oQ',
-  };
+  apiUrl: 'https://api.whimbean.com',
+  mapboxToken: 'pk.eyJ1Ijoia25pYXpldmdlbnkiLCJhIjoiY21zY2Z5amt4MG9lOTJ4cXQxM2phOWJiZSJ9.e6MWfw6ykMAtXWi70Qh7oQ',
+  posthogKey: 'phc_rbk2qj3dMmoY2LL2jTgKYhJXfmDwiaQZY79KD72DiC9A',
+  posthogHost: 'https://us.i.posthog.com',
+}
